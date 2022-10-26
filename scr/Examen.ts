ce6e1cd8f1b3d7a07291be2a32247268a1c972a6
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-10-25 21:27:57 -0300

    Add files via upload

---
+++ scr/Examen.ts
@@ -0,0 +1,32 @@
+import { Pregunta } from "./Pregunta";
+
+export class Examen extends Pregunta {
+    private preguntas: Pregunta[];
+    private tema: string;
+    private puntajeAprobacion: number;
+
+    constructor(pconsigna:string, popciones:string[], popcionCorrecta:number, ppuntaje:number, tema: string, puntaje: number) {
+        super(pconsigna, popciones, popcionCorrecta, ppuntaje);
+    
+        this.tema=tema;
+        this.puntajeAprobacion=puntaje;
+    }
+
+    public addPregunta (ppregunta:Pregunta):void {
+        this.preguntas.push(ppregunta);
+    }
+
+    /*public equals(o:object):boolean{ 
+        
+        let correcto:boolean=false;
+        
+       //if (this.opcionCorrecta===prespuesta){
+             correcto=true;
+        //}
+        return correcto; 
+    }*/
+
+    public getTema():string{
+        return this.tema;
+    }
+}
